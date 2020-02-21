10b58912aaad27312e8deae065b99a5d0e0eec3a
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-21 19:49:01 +0530

    Add Post component with data fetching.

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import BlogContext from "../contexts/blog.context";
 
-const Post = () => {
-  return <div>Post</div>;
+const Post = props => {
+  const { getPost, currentBlogPost } = useContext(BlogContext);
+
+  let { id } = useParams();
+
+  useEffect(() => {
+    getPost(id);
+  }, [id]);
+
+  console.log(currentBlogPost);
+  return (
+    <div>
+      {currentBlogPost ? (
+        <>
+          <div
+            className="post-image"
+            style={{
+              backgroundImage: `url('${currentBlogPost.image}')`,
+              width: "100%",
+              height: "300px",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              position: "relative"
+            }}
+          ></div>
+          <div className="post-content">
+            <h2>{currentBlogPost.title}</h2>
+            <p>{currentBlogPost.content}</p>
+          </div>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 };
 
 export default Post;

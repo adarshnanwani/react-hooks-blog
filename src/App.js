959ed3e6f0229ae9e35505f785dab560670966bc
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-21 17:57:46 +0530

    Add hero component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />>
+    </div>
+  );
 }
 
 export default App;
